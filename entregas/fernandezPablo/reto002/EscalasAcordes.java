4acce0f77b3ed7301d9a14e90a5e46f0b81a0f6d
pablofernandez2-cmd <pablo.fernandez2@alumnos.uneatlantico.es>
2025-02-17 15:39:56 +0100

    Agrego configuración inicial del proyecto y constantes de notas e intervalos

--- entregas/fernandezPablo/reto002/EscalasAcordes.java
+++ entregas/fernandezPablo/reto002/EscalasAcordes.java
@@ -2,8 +2,22 @@ package entregas.fernandezPablo.reto002;
 
 import java.util.Scanner;
 
-public class EscalasAcordes {
-    public static void main(String[] args) {
-
-    }
+class EscalasAcordes {
+    static final String[] NOTAS = { "DO", "DO#", "RE", "RE#", "MI", "FA", "FA#", "SOL", "SOL#", "LA", "LA#", "SI" };
+    static final int SEMITONO = 1,  TONO =  SEMITONO + SEMITONO, TONOYMEDIO = TONO + SEMITONO;
+    static final int[][] ESCALAS = 
+    {
+        { TONO, TONO, SEMITONO, TONO, TONO, TONO, SEMITONO },
+        { TONO, SEMITONO, TONO, TONO, SEMITONO, TONO, TONO },
+        { TONO, SEMITONO, TONO, TONO, SEMITONO, TONOYMEDIO, SEMITONO },
+        { TONO, SEMITONO, TONO, TONO, TONO, TONO, SEMITONO },
+        { TONO, TONO, TONOYMEDIO, TONO, TONOYMEDIO },
+        { TONOYMEDIO, TONO, TONO, TONOYMEDIO, TONO },
+        { TONO, SEMITONO, TONO, TONO, TONO, SEMITONO, TONO },
+        { SEMITONO, TONO, TONO, TONO, SEMITONO, TONO, TONO },
+        { TONO, TONO, TONO, SEMITONO, TONO, TONO, SEMITONO },
+        { TONO, TONO, SEMITONO, TONO, TONO, SEMITONO, TONO },
+        { SEMITONO, TONO, TONO, SEMITONO, TONO, TONO, TONO },
+        { TONO, TONO, TONO, TONO, TONO, TONO }
+    };
 }
