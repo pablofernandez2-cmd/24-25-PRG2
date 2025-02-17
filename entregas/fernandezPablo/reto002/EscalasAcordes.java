fc9ed58c4f91f584b45788cab02d35fa740a5309
pablofernandez2-cmd <pablo.fernandez2@alumnos.uneatlantico.es>
2025-02-17 22:05:48 +0100

    Correcion de Errores

--- entregas/fernandezPablo/reto002/EscalasAcordes.java
+++ entregas/fernandezPablo/reto002/EscalasAcordes.java
@@ -4,22 +4,23 @@ import java.util.Scanner;
 
 class EscalasAcordes {
     static final String[] NOTAS = { "DO", "DO#", "RE", "RE#", "MI", "FA", "FA#", "SOL", "SOL#", "LA", "LA#", "SI" };
-    static final int SEMITONO = 1,  TONO =  SEMITONO + SEMITONO, TONOYMEDIO = TONO + SEMITONO;
-    static final int[][] ESCALAS = 
-    
-        { TONO, TONO, SEMITONO, TONO, TONO, TONO, SEMITONO },
-        { TONO, SEMITONO, TONO, TONO, SEMITONO, TONO, TONO },
-        { TONO, SEMITONO, TONO, TONO, SEMITONO, TONOYMEDIO, SEMITONO },
-        { TONO, SEMITONO, TONO, TONO, TONO, TONO, SEMITONO },
-        { TONO, TONO, TONOYMEDIO, TONO, TONOYMEDIO },
-        { TONOYMEDIO, TONO, TONO, TONOYMEDIO, TONO },
-        { TONO, SEMITONO, TONO, TONO, TONO, SEMITONO, TONO },
-        { SEMITONO, TONO, TONO, TONO, SEMITONO, TONO, TONO },
-        { TONO, TONO, TONO, SEMITONO, TONO, TONO, SEMITONO },
-        { TONO, TONO, SEMITONO, TONO, TONO, SEMITONO, TONO },
-        { SEMITONO, TONO, TONO, SEMITONO, TONO, TONO, TONO },
-        { TONO, TONO, TONO, TONO, TONO, TONO }
-    
+    static final int SEMITONO = 1, TONO = SEMITONO + SEMITONO, TONOYMEDIO = TONO + SEMITONO;
+    static final int[][] ESCALAS = {
+
+            { TONO, TONO, SEMITONO, TONO, TONO, TONO, SEMITONO },
+            { TONO, SEMITONO, TONO, TONO, SEMITONO, TONO, TONO },
+            { TONO, SEMITONO, TONO, TONO, SEMITONO, TONOYMEDIO, SEMITONO },
+            { TONO, SEMITONO, TONO, TONO, TONO, TONO, SEMITONO },
+            { TONO, TONO, TONOYMEDIO, TONO, TONOYMEDIO },
+            { TONOYMEDIO, TONO, TONO, TONOYMEDIO, TONO },
+            { TONO, SEMITONO, TONO, TONO, TONO, SEMITONO, TONO },
+            { SEMITONO, TONO, TONO, TONO, SEMITONO, TONO, TONO },
+            { TONO, TONO, TONO, SEMITONO, TONO, TONO, SEMITONO },
+            { TONO, TONO, SEMITONO, TONO, TONO, SEMITONO, TONO },
+            { SEMITONO, TONO, TONO, SEMITONO, TONO, TONO, TONO },
+            { TONO, TONO, TONO, TONO, TONO, TONO }
+
+    };
 
     public static void main(String[] args) {
         String nota = preguntarNota();
@@ -40,14 +41,13 @@ class EscalasAcordes {
         System.out.println();
     }
 
-    static String[] construirAcorde(String[] escala) 
-    {
-        return new String[] { escala[0], escala[2], escala[4] }; 
+    static String[] construirAcorde(String[] escala) {
+        return new String[] { escala[0], escala[2], escala[4] };
     }
 
     static String[] construirEscala(String nota, int[] intervalos) {
         int posicionEnNotas = obtenerIndiceNota(nota);
-        String[] escala = new String[intervalos.length + 1]; 
+        String[] escala = new String[intervalos.length + 1];
 
         for (int i = 0; i < escala.length; i++) {
             escala[i] = NOTAS[posicionEnNotas];
